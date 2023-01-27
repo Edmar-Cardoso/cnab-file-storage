2489083f9fe8069c838206bba91107917bc09cf2
Edmar Cardoso <ed2019junior@gmail.com>
2023-01-27 19:51:43 -0300

    feat: created-relations-app

--- files/models.py
+++ files/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class File(models.Model):
-    transaction_type = models.IntegerField()
+    transaction_type = models.CharField(max_length=30)
     transaction_at = models.DateField()
     value = models.FloatField()
-    cpf = models.CharField(max_length=11)
     card = models.CharField(max_length=12)
     transaction_hour = models.CharField(max_length=6)
-    owner = models.CharField(max_length=14)
-    name = models.CharField(max_length=19)
     file = models.CharField(max_length=8100)
+
+    store = models.ForeignKey(
+        "stores.Store", on_delete=models.CASCADE, related_name="files"
+    )
--- files/serializers.py
+++ files/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import File
+from stores.models import Store
 from datetime import datetime
 
 class FileSerializer(serializers.ModelSerializer):
@@ -11,29 +12,26 @@ class FileSerializer(serializers.ModelSerializer):
             'transaction_type', 
             'transaction_at', 
             'value', 
-            'cpf', 
             'card', 
             'transaction_hour',
-            'owner',
-            'name',
+            'store'
         ]
         read_only_fields = [
             'id', 
             'transaction_type', 
             'transaction_at', 
-            'value', 
-            'cpf', 
+            'value',      
             'card', 
             'transaction_hour',
-            'owner',
-            'name',
+            'store'
         ]
         extra_kwargs = {
             "file": {"write_only":True}
-        }
+        }   
+        depth = 1
 
     def create(self, validadeted_data: dict) -> dict:
-        # placing a marker to separate the string
+        # placing a marker to separate the string:
 
         new_string = ""
         for x in range(len(validadeted_data["file"])):
@@ -42,22 +40,60 @@ class FileSerializer(serializers.ModelSerializer):
             else:
                 new_string += validadeted_data["file"][x]
 
-        #persisting data in the database
+        # finding transaction type:
+
+        def transaction_type(int):
+            if int == 1:
+                return "débito"
+            elif int == 2:
+                return "boleto"
+            elif int == 3:
+                return "financiamento"
+            elif int == 4:
+                return "crédito"
+            elif int == 5:
+                return "recebimento empréstimo"
+            elif int == 6:
+                return "vendas"
+            elif int == 7:
+                return "recebimento TED"
+            elif int == 8:
+                return "recebimento DOC"
+            elif int == 9:
+                return "alugel"
+
+        # persisting data in the database:
         
         array_files = new_string.split("##")
 
         for file in array_files:
-            data_file = {
-                'transaction_type': int(file[0:1]), 
-                'transaction_at': datetime.strptime(f'{file[1:5]}-{file[5:7]}-{file[7:9]}', '%Y-%m-%d').date(),
-                'value': float(int(file[9:19])/100),
-                'cpf': file[19:30],
-                'card': file[30:42],
-                'transaction_hour': f'{file[42:44]}:{file[44:46]}:{file[46:48]}',
+            data_transaction_store = { 
                 'owner': file[48:62],
                 'name': file[62: 81],
+                'cpf': file[19:30],
+            }
+
+            data_file = {
+                'transaction_type': transaction_type(int(file[0:1])), 
+                'transaction_at': datetime.strptime(f'{file[1:5]}-{file[5:7]}-{file[7:9]}', '%Y-%m-%d').date(),
+                'value': float(int(file[9:19])/100),
+                'card': file[30:42],
+                'transaction_hour': f'{file[42:44]}:{file[44:46]}:{file[46:48]}',
                 'file': ""
             }
+
+            transaction_store = Store.objects.get_or_create(**data_transaction_store)[0]
+
+            # updating the store total
+
+            if int(file[0:1]) == 2 or int(file[0:1]) == 3 or int(file[0:1]) == 9:
+                transaction_store.total = transaction_store.total - data_file['value']
+                transaction_store.save()
+            else:
+                transaction_store.total = transaction_store.total + data_file['value']
+                transaction_store.save()
+
+            data_file['store'] = transaction_store
 
             File.objects.create(**data_file)
 
